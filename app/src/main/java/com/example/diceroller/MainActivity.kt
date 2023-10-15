9b5ba69f7a88e1f5123a57ec750447df4b43e096
Moonlight200701 <93810080+Moonlight200701@users.noreply.github.com>
2023-10-15 16:50:21 +0700

    Adding another Dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -27,9 +27,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val dice = Dice(6)
+        val dice2 = Dice(6)
         val diceRoll = dice.roll()
+        val diceRoll2 = dice2.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
+        val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView.text = diceRoll.toString()
+        resultTextView2.text = diceRoll2.toString()
     }
 }
 
